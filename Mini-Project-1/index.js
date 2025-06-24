d85dab5fbdeccc08c42877dc6f00c46424b69a49
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-24 15:58:23 +0530

    Add user profile view and enhance login functionality

--- Mini-Project-1/index.js
+++ Mini-Project-1/index.js
@@ -17,6 +17,7 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.render('index');
 })
+
 // Registring
 app.post('/register', async (req, res) => {
     const user = await User.findOne({ email: req.body.email })
@@ -35,6 +36,7 @@ app.post('/register', async (req, res) => {
         })
     })
 })
+
 // login
 app.get('/login', (req, res) => {
     res.render('login');
@@ -48,31 +50,43 @@ app.post('/login', async (req, res) => {
     if (result) {
       const token = jwt.sign({ email, userid: user._id }, "secretkey");
       res.cookie("token", token);
-      return res.status(200).send('Login successful');
+      return res.status(200).redirect('/profile');
     } else {
       return res.status(401).send('Incorrect password');
     }
   });
 });
+
 // logout
 app.get('/logout', (req, res) => {
     res.clearCookie('token');
     res.redirect('/login');
 })
+
 // create
 app.post('/create', (req, res) => {
     res.send('Hello World!')
 })
 
-
-app.get('/pro', isLoggedin, (req, res) => {
-    console.log(req.user);
-    res.render('index');
+// profile
+app.get('/profile', isLoggedin, async (req, res) => {
+  const user = await User.findOne({email: req.user.email}).populate('posts')
+  res.render('profile', {user: user});
+})
+app.post('/post', isLoggedin, async (req, res) => {
+  const user = await User.findOne({email: req.user.email})
+  const post = await Post.create({
+    user: user._id,
+    content: req.body.content
+  })
+  user.posts.push(post._id)
+  await user.save()
+  res.redirect('/profile')
 })
 
 function isLoggedin(req, res, next) {
   if (req.cookies.token === undefined) {
-    res.send('You must be logged in');
+    res.redirect('/login');
   } else {
     const token = jwt.verify(req.cookies.token, "secretkey");
     req.user = token;
